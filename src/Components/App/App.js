8ae8aa7ba2bccb50b17accd3c25d03d5ca2b6822
sudo-alex1990 <alexrfarnes@gmail.com>
2021-03-02 19:23:46 +0800

    Add the add and remove functionality to Results and Playlist components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,11 +3,33 @@ import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar.js';
 import { SearchResults } from '../SearchResults/SearchResults.js';
 import { Playlist } from '../Playlist/Playlist.js';
+import { Track } from '../Track/Track';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searchResults: [{name:'Fly me to the moon', artist: 'Frank Sinatra', album: 'It Might As Well Be Swing', id: '1Mxqyy3pSjf8kZZL4QVxS0' }, {name: 'New York New York', aritst: 'Frank Sinatra', album: 'Duets', id: '2lolQgalUvZDfp5vvVtTYV'}, {name: 'Young at Heart', artist: 'Frank Sinatra', album: 'Sinatra\'s Sinatra', id: '3HBuwtSVm028hyFuwuTiJ5'}] }
+    this.state = { 
+      searchResults: 
+        [{name:'Fly me to the moon', artist: 'Frank Sinatra', album: 'It Might As Well Be Swing', id: '1Mxqyy3pSjf8kZZL4QVxS0' }, {name: 'New York New York', aritst: 'Frank Sinatra', album: 'Duets', id: '2lolQgalUvZDfp5vvVtTYV'}, {name: 'Young at Heart', artist: 'Frank Sinatra', album: 'Sinatra\'s Sinatra', id: '3HBuwtSVm028hyFuwuTiJ5'}], 
+      playlistName: 'Smooth Blues',  
+      playlistTracks: 
+        [{name:'Fly me to the moon', artist: 'Frank Sinatra', album: 'It Might As Well Be Swing', id: '1Mxqyy3pSjf8kZZL4QVxS0' }, {name: 'New York New York', aritst: 'Frank Sinatra', album: 'Duets', id: '2lolQgalUvZDfp5vvVtTYV'}]
+      }
+      this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return ;
+    }
+    this.setState({playlistTracks: [...this.state.playlistTracks, track]})
+  }
+
+  removeTrack(track) {
+    let newPlaylistTracks = this.state.playlistTracks.filter(prevTracks => prevTracks.id !== track.id);
+    
+    this.setState({playlistTracks: newPlaylistTracks});
   }
 
   render() {
@@ -17,8 +39,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults}/>
+            <Playlist onRemove={this.removeTrack} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,7 +7,7 @@ export class TrackList extends React.Component {
         return (
             <div className="TrackList">
                 {this.props.tracks.map(track => {
-                    return <Track id={track.id} track={track} />
+                    return <Track onRemove={this.props.onRemove} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} key={track.id} track={track} />
                 })}
             </div>
         )
